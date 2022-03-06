7cb5bdb3cc1a8a7bbc3285325d046fd5681416f2
Sudalaimani09 <msudalaimani92@gmail.com>
2022-03-06 15:50:27 +0530

    MANI914

---
+++ EmpName.java
@@ -0,0 +1,13 @@
+package org.base;
+
+public class EmpName {
+
+	private void employeeName(String name) {
+		System.out.println("Employee name is : " + name);
+	}
+
+	public static void main(String[] args) {
+		EmpName a = new EmpName();
+		a.employeeName("SudalaiMani");
+	}
+}
