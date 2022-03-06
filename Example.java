7250a2dfd2d6d2e878c0344fe189c71f338c72fb
Sudalaimani09 <msudalaimani92@gmail.com>
2022-03-06 15:03:42 +0530

    Sudalai92

---
+++ Example.java
@@ -0,0 +1,12 @@
+package org.base;
+
+public class Example {
+
+	private void employeeName(String name) {
+		System.out.println("Employee name is : " + name);
+	}
+	public static void main(String[] args) {
+		Example a = new Example();
+		a.employeeName("SudalaiMani");
+	}
+}
